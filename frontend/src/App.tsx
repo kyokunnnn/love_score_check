10a0ab78066b243fb3b19dfeaa8e530f481fbe06
kyokunnnn <ikeckyo@gmail.com>
2025-06-15 16:12:24 +0900

    筆者紹介ページをざっと実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import CategorySelectPage from './pages/CategorySelectPage';
 import QuizResult from './pages/QuizResult';
 import QuizByCategoryPage from './pages/QuizByCategoryPage';
 import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="about" element={<AboutPage />} />
         <Route path="/category-select" element={<CategorySelectPage />} />
         <Route path="/quiz/:categoryId" element={<QuizByCategoryPage />} />
         <Route path="/quiz/result" element={<QuizResult />} />

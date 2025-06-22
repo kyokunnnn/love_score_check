1972a62ba11d0401fbfcdab2c0c29542aec8de74
kyokunnnn <ikeckyo@gmail.com>
2025-06-22 18:08:28 +0900

    id毎にクイズを出すページを削除。 API側はコメントアウト

--- api/src/routes/questions.ts
+++ api/src/routes/questions.ts
@@ -4,43 +4,43 @@ import { QuizChoiceRow } from "../type/questions";
 
 const router = Router();
 
-// 特定の質問を選択肢付きで取得
-router.get("/:id", async (req, res) => {
-  try {
-    const quizId = Number(req.params.id);
+// // 特定の質問を選択肢付きで取得
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const quizId = Number(req.params.id);
 
-    const [rows] = await pool.query<QuizChoiceRow[]>(
-      `
-      SELECT
-        q.id AS question_id,
-        q.text AS question_text,
-        c.id AS choice_id,
-        c.choice_text AS choice_text,
-        c.is_correct AS is_correct
-      FROM questions q
-      LEFT JOIN quiz_choices c ON q.id = c.quiz_id
-      WHERE q.id = ?
-      `,
-      [quizId]
-    )
+//     const [rows] = await pool.query<QuizChoiceRow[]>(
+//       `
+//       SELECT
+//         q.id AS question_id,
+//         q.text AS question_text,
+//         c.id AS choice_id,
+//         c.choice_text AS choice_text,
+//         c.is_correct AS is_correct
+//       FROM questions q
+//       LEFT JOIN quiz_choices c ON q.id = c.quiz_id
+//       WHERE q.id = ?
+//       `,
+//       [quizId]
+//     )
 
-    // 整形してクライアントに返す形式にする
-    const question = {
-      questionId: rows[0].question_id,
-      questionText: rows[0].question_text,
-      choices: rows.map((row: any) => ({
-        id: row.choice_id,
-        text: row.choice_text,
-        isCorrect: row.is_correct,
-      }))
-    };
+//     // 整形してクライアントに返す形式にする
+//     const question = {
+//       questionId: rows[0].question_id,
+//       questionText: rows[0].question_text,
+//       choices: rows.map((row: any) => ({
+//         id: row.choice_id,
+//         text: row.choice_text,
+//         isCorrect: row.is_correct,
+//       }))
+//     };
 
-    res.json(question);
-  } catch (error) {
-    console.error("Database error:", error);
-    res.status(500).json({ error: "Database error" });
-  }
-});
+//     res.json(question);
+//   } catch (error) {
+//     console.error("Database error:", error);
+//     res.status(500).json({ error: "Database error" });
+//   }
+// });
 
 // カテゴリー毎に複数のクイズと選択肢をまとめて取得
 router.get("/", async (req, res) => {
